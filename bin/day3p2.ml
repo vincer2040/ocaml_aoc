a1dd2bce9311789f0ab81e56cb03a2d048a00177
boreddad <ronnebaumfinances@gmail.com>
2023-08-02 20:27:33 -0400

    fancy ocaml

--- bin/day3p2.ml
+++ bin/day3p2.ml
@@ -13,24 +13,21 @@ let rec get_three list =
 let get_common list =
   let map_first = MyMap.empty in
   let map_second = MyMap.empty in
-  let first = List.nth list 0 in
-  let second = List.nth list 1 in
-  let third = List.nth list 2 in
   let map_first_c =
-    String.fold_left (fun acc char -> MyMap.add char 0 acc) map_first first
+    List.nth list 0
+    |> String.fold_left (fun acc char -> MyMap.add char 0 acc) map_first
   in
   let map_second_c =
-    String.fold_left
-      (fun acc char ->
-        if MyMap.mem char map_first_c then MyMap.add char 0 acc else acc)
-      map_second
-      second
+    List.nth list 1
+    |> String.fold_left
+         (fun acc char ->
+           if MyMap.mem char map_first_c then MyMap.add char 0 acc else acc)
+         map_second
   in
   let result = ref None in
-  String.iter
-    (fun c ->
-      if MyMap.mem c map_second_c && !result = None then result := Some c)
-    third;
+  List.nth list 2
+  |> String.iter (fun c ->
+       if MyMap.mem c map_second_c && !result = None then result := Some c);
   !result
 ;;
 
